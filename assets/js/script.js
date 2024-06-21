3ba4e0d83429ebec1e7e26b0e3fb74a0798b7f71
3Lsome <luckyllemos0909@gmail.com>
2024-06-21 18:20:39 +0800

    Update remove function on scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,6 @@
 let navItems = document.querySelector("#navSession")
 let navItems1 = document.querySelector("#navSession1")
 
-console.log(navItems)
-console.log(navItems1)
 let userToken = localStorage.getItem("token")
 if (navItems && navItems1) {
 	if (!userToken) {
@@ -27,40 +25,5 @@ if (navItems && navItems1) {
 		</li>
 	
 		`
-	}
-
-	window.onscroll = function () { myFunction() };
-
-	// Get the navbar
-	var navbar = document.getElementById("navbar");
-
-	// Get the offset position of the navbar
-	var sticky = navbar.offsetTop;
-
-	// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-	function myFunction() {
-		if (window.pageYOffset >= sticky) {
-			navbar.classList.add("sticky")
-		} else {
-			navbar.classList.remove("sticky");
-		}
-	}
-} else {
-
-	window.onscroll = function () { myFunction() };
-
-	// Get the navbar
-	var navbar = document.getElementById("navbar");
-
-	// Get the offset position of the navbar
-	var sticky = navbar.offsetTop;
-
-	// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-	function myFunction() {
-		if (window.pageYOffset >= sticky) {
-			navbar.classList.add("sticky")
-		} else {
-			navbar.classList.remove("sticky");
-		}
 	}
 }
