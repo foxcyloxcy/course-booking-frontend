25ee8b57cbf0b82e7b6228e2e933bac237750911
3Lsome <luckyllemos0909@gmail.com>
2024-06-21 13:38:36 +0800

    Update fix navbar in showing profile and logout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,66 @@
 let navItems = document.querySelector("#navSession")
 let navItems1 = document.querySelector("#navSession1")
 
-
+console.log(navItems)
+console.log(navItems1)
 let userToken = localStorage.getItem("token")
+if (navItems && navItems1) {
+	if (!userToken) {
+		navItems.innerHTML =
+			`
+		<li class="nav-item">
+		<a href="./login.html" class="nav-link active">Log In</a>
+		</li>
+		`
+	} else {
+		navItems1.innerHTML =
+			`
+		<li class="nav-item">
+		<a href="./profile.html" class="nav-link active">Profile</a>
+		</li>
+	
+		`
+		navItems.innerHTML =
+			`
+		<li class="nav-item">
+		<a href="./logout.html" class="nav-link active">Log Out</a>
+		</li>
+	
+		`
+	}
 
-if(!userToken){
-	navItems.innerHTML =
-	`
-	<li class="nav-item">
-	<a href="./login.html" class="nav-link active">Log In</a>
-	</li>
-	`
-}else{
-	navItems1.innerHTML =
-	`
-	<li class="nav-item">
-	<a href="./profile.html" class="nav-link active">Profile</a>
-	</li>
+	window.onscroll = function () { myFunction() };
 
-	`
-	navItems.innerHTML =
-	`
-	<li class="nav-item">
-	<a href="./logout.html" class="nav-link active">Log Out</a>
-	</li>
+	// Get the navbar
+	var navbar = document.getElementById("navbar");
 
-	`
-}
+	// Get the offset position of the navbar
+	var sticky = navbar.offsetTop;
 
-window.onscroll = function() {myFunction()};
+	// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+	function myFunction() {
+		if (window.pageYOffset >= sticky) {
+			navbar.classList.add("sticky")
+		} else {
+			navbar.classList.remove("sticky");
+		}
+	}
+} else {
 
-// Get the navbar
-var navbar = document.getElementById("navbar");
+	window.onscroll = function () { myFunction() };
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+	// Get the navbar
+	var navbar = document.getElementById("navbar");
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
+	// Get the offset position of the navbar
+	var sticky = navbar.offsetTop;
+
+	// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+	function myFunction() {
+		if (window.pageYOffset >= sticky) {
+			navbar.classList.add("sticky")
+		} else {
+			navbar.classList.remove("sticky");
+		}
+	}
 }
