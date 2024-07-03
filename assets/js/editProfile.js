0dc51eedbb43798efae5014cb635db5dec563a4a
3Lsome <luckyllemos0909@gmail.com>
2024-07-03 14:20:58 +0800

    Update use typescript formatter, and queryselector for cancelButton

--- assets/js/editProfile.js
+++ assets/js/editProfile.js
@@ -8,55 +8,56 @@ let lastName = document.querySelector("#lastName")
 let email = document.querySelector("#userEmail")
 let mobileNo = document.querySelector("#contactNumber")
 let editProfile = document.querySelector("#editProfileButton")
+let cancelButton = document.querySelector("#cancelButton")
 
-  fetch('http://localhost:3000/api/users/details', {
-    headers: {
-      'Authorization': `Bearer ${token}`
-    }
-  })
+fetch('http://localhost:3000/api/users/details', {
+  headers: {
+    'Authorization': `Bearer ${token}`
+  }
+})
   .then(res => res.json())
   .then(data => {
-  console.log(data)
+    console.log(data)
 
-      firstName.placeholder = data.firstName
-      lastName.placeholder = data.lastName
-      email.placeholder = data.email
-      mobileNo.placeholder = data.mobileNo
+    firstName.placeholder = data.firstName
+    lastName.placeholder = data.lastName
+    email.placeholder = data.email
+    mobileNo.placeholder = data.mobileNo
 
     document.querySelector("#editProfile").addEventListener("submit", (e) => {
-        e.preventDefault()
+      e.preventDefault()
 
-        let unoName = firstName.value
-        let surName = lastName.value
-        let elecMail = email.value
-        let cpNo = mobileNo.value
-        let userId = data._id
+      let unoName = firstName.value
+      let surName = lastName.value
+      let elecMail = email.value
+      let cpNo = mobileNo.value
+      let userId = data._id
 
 
-        fetch('http://localhost:3000/api/users/update', {
-          method: "PUT",
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`
-          },
-          body: JSON.stringify({
-            userId: userId,
-            firstName: unoName,
-            lastName: surName,
-            email: elecMail,
-            mobileNo: cpNo
-          })
+      fetch('http://localhost:3000/api/users/update', {
+        method: "PUT",
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({
+          userId: userId,
+          firstName: unoName,
+          lastName: surName,
+          email: elecMail,
+          mobileNo: cpNo
         })
+      })
         .then(res => res.json())
         .then(data => {
-          if(data === true){
+          if (data === true) {
             window.location.replace('./profile.html')
-          }else{
+          } else {
             alert("Something went wrong.")
           }
         })
-      })
-})
+    })
+  })
 
 
 
