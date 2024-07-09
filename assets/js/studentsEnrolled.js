174c1ec010e457176c2596f205f1508366f9af30
3Lsome <luckyllemos0909@gmail.com>
2024-07-09 08:58:31 +0800

    Update change format

--- assets/js/studentsEnrolled.js
+++ assets/js/studentsEnrolled.js
@@ -5,57 +5,57 @@ let adminUser = localStorage.getItem("isAdmin")
 import baseURL from "../baseURL/baseURL.js";
 let swalContainer = document.querySelector("#swalContainer")
 
-if(token === null){
+if (token === null) {
   alert("You cannot do that! \n Login first!")
   window.location.replace("./login.html")
-}else{
+} else {
 
-  if(adminUser === "false"){
-  alert("You cannot do that!")
-  window.location.replace("./courses.html")
-}else{
-  
-  fetch(`${baseURL}api/courses/${courseId}`)
-.then(res => res.json())
-.then(data => {
-  console.log(data)
+  if (adminUser === "false") {
+    alert("You cannot do that!")
+    window.location.replace("./courses.html")
+  } else {
 
-  let enrolleesData;
+    fetch(`${baseURL}api/courses/${courseId}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+
+        let enrolleesData;
 
 
-      // console.log(enrollees)
-      if(data.enrollees.length === 0){
+        // console.log(enrollees)
+        if (data.enrollees.length === 0) {
           alert("No enrolled student on this course! \n Return to courses.")
           window.location.replace('./courses.html')
-      }else{
-        data.enrollees.map(enrolled => {
+        } else {
+          data.enrollees.map(enrolled => {
 
-        fetch(`${baseURL}api/users/all`,{
-          headers:{
-            'Authorization': `Bearer ${token}`
-          }
-        })
-        .then(res => res.json())
-        .then(data => {
-          // console.log(data)
-          enrolleesData = data.map(users => {
-          // console.log(users)
-          if(enrolled.userId !== users._id){
-                enrolleesData = "No enrollees."
-          }else{
-            enrolleesContainer.innerHTML +=
-          `
+            fetch(`${baseURL}api/users/all`, {
+              headers: {
+                'Authorization': `Bearer ${token}`
+              }
+            })
+              .then(res => res.json())
+              .then(data => {
+                // console.log(data)
+                enrolleesData = data.map(users => {
+                  // console.log(users)
+                  if (enrolled.userId !== users._id) {
+                    enrolleesData = "No enrollees."
+                  } else {
+                    enrolleesContainer.innerHTML +=
+                      `
             <ul>
               <li>${users.firstName} ${users.lastName}</li>
             </ul>
          `
-          }
-          })
+                  }
+                })
 
-        })
+              })
+          })
+        }
       })
-    }
-  })
-}
+  }
 }
 
